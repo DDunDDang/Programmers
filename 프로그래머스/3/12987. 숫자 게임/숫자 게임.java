f034dcb3e1ad68721c3ac2039ee05943af39099a
DDunDDang <120348865+DDunDDang@users.noreply.github.com>
2024-09-05 19:34:20 +0900

    [level 3] Title: 숫자 게임, Time: 120.11 ms, Memory: 63.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int answer = 0;
+        int indexB = B.length - 1;
+        
+        for (int i = A.length - 1; i >= 0; i--) {
+            if (B[indexB] > A[i]) {
+                answer++;
+                indexB--;
+            }
+        }
+        
+        return answer;
+    }
+}
