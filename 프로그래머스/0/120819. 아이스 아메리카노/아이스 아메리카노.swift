7499b5ebedfffe4c1400a001b1d0166ecce97918
DDunDDang <120348865+DDunDDang@users.noreply.github.com>
2024-09-25 10:23:33 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ money:Int) -> [Int] {
+    var result : [Int] = [Int]()
+    result.append(money / 5500)
+    result.append(money % 5500)
+    
+    return result
+}
